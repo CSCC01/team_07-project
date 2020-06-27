26d86127d18cd13f3f16724cbc9e6e354e33d024
Vialor <42876369+Vialor@users.noreply.github.com>
2020-06-26 20:00:02 -0400

    finish promotion time

--- frontend-restaurant-owner/src/CreatePromotion.js
+++ frontend-restaurant-owner/src/CreatePromotion.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import SubtaskBtns from './components/SubtaskBtns';
-import PromotionTime from './components/PromotionTime'
+import PromotionTime from './components/PromotionTime';
 
 function CreatePromotion() {
   return (
