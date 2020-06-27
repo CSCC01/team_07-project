f42e36fea7ee56fa1f66f50549bdf010e148326b
1231joe1231 <zhuangruixin1231@gmail.com>
2020-06-26 20:58:16 -0400

    Disable proptype check in eslint

--- frontend-restaurant-owner/.eslintrc.js
+++ frontend-restaurant-owner/.eslintrc.js
@@ -20,6 +20,7 @@ module.exports = {
   rules: {
     'no-unused-vars': ['warn', { args: 'none', argsIgnorePattern: 'req|res|next|val' }],
     'prettier/prettier': ['error'],
+    "react/prop-types": 0
   },
   settings: {
     react: {
