b7587d422387dcef15f39f956a36b043f0cd6830
Yifei Yin <yifeiyin@foxmail.com>
2020-07-18 17:05:51 -0400

    Add popup to preset templates (#69)
    * Add ModalPopUp component
    
    * Replace inputs with TextFields
    
    * Rename file and component for SubtaskButtons
    
    - Also add min and max to inputs
    
    * Replace addBtn to addButton for consistency
    
    * Use better wording for preset templates
    
    * Reorganize preset templates and add more stylings
    
    * Add default case to switch statement

--- frontend-restaurant-owner/src/CreatePromotion/CreateSubtask/PresetTemplates.js
+++ frontend-restaurant-owner/src/CreatePromotion/CreateSubtask/PresetTemplates.js
@@ -1,8 +1,6 @@
 import React from 'react';
+import { TextField, Dialog, DialogContent, Button } from '@material-ui/core';
 import styles from './PresetTemplates.module.css';
-import NVisit from './NVisit';
-import XItems from './XItems';
-import XDollars from './XDollars';
 
 class PresetTemplates extends React.Component {
   constructor(props) {
@@ -41,6 +39,30 @@ class PresetTemplates extends React.Component {
 
   handleItemNameChange = (e) => {
     this.setState({ itemName: e.target.value });
+  };
+
+  onPresetComplete = (type = '') => {
+    if (!['items', 'dollars', 'visits'].includes(type)) console.error('Unexpected type: ' + type);
+
+    this.props.deleteAllTasks();
+    switch (type) {
+      case 'items':
+        for (let i = 0; i < this.state.x; i++)
+          this.props.addButton(`Buy 1 ${this.state.itemName}.`);
+        break;
+
+      case 'dollars':
+        this.props.addButton(`Get reward for spending ${this.state.x} dollars on an order.`);
+        break;
+
+      case 'visits':
+        for (let i = 0; i < this.state.x; i++) this.props.addButton('Complete one visit.');
+        break;
+
+      default:
+        throw new Error('Unreachable');
+    }
+    this.hideAndClear();
   };
 
   render() {
@@ -86,50 +108,63 @@ class PresetTemplates extends React.Component {
           </p>
         </div>
 
-        {this.state.selectedTemplate === 1 && (
-          <div>
-            <p>Enter # of visits</p>
-            <input type="number" onChange={this.handleXChange}></input>
-            <NVisit
-              addBtn={this.props.addBtn}
-              deleteAllTasks={this.props.deleteAllTasks}
-              count={this.state.x}
-              hideAndClear={this.hideAndClear}
-            />
-          </div>
-        )}
+        <Dialog open={this.state.selectedTemplate !== 0} onClose={this.hideAndClear}>
+          <DialogContent dividers>
+            {this.state.selectedTemplate === 1 && (
+              <div>
+                <p>Enter number of visits</p>
+                <TextField
+                  variant="outlined"
+                  type="number"
+                  inputProps={{ min: 1, max: 20 }}
+                  onChange={this.handleXChange}
+                />
+                <SubmitButton onClick={() => this.onPresetComplete('visits')} />
+              </div>
+            )}
 
-        {this.state.selectedTemplate === 2 && (
-          <div>
-            <p>Enter # of items need to be ordered.</p>
-            <input type="number" onChange={this.handleXChange}></input>
-            <p>Enter the name of item need to be ordered.</p>
-            <input type="text" onChange={this.handleItemNameChange}></input>
-            <XItems
-              addBtn={this.props.addBtn}
-              deleteAllTasks={this.props.deleteAllTasks}
-              itemCount={this.state.x}
-              itemName={this.state.itemName}
-              hideAndClear={this.hideAndClear}
-            />
-          </div>
-        )}
+            {this.state.selectedTemplate === 2 && (
+              <div>
+                <p>Enter number of items need to be ordered.</p>
+                <TextField
+                  variant="outlined"
+                  type="number"
+                  inputProps={{ min: 1, max: 20 }}
+                  onChange={this.handleXChange}
+                />
+                <p>Enter the name of item need to be ordered.</p>
+                <TextField type="text" onChange={this.handleItemNameChange} />
+                <SubmitButton onClick={() => this.onPresetComplete('items')} />
+              </div>
+            )}
 
-        {this.state.selectedTemplate === 3 && (
-          <div>
-            <p>Enter the money need to spend on an order.</p>
-            <input type="number" onChange={this.handleXChange}></input>
-            <XDollars
-              addBtn={this.props.addBtn}
-              deleteAllTasks={this.props.deleteAllTasks}
-              dollars={this.state.x}
-              hideAndClear={this.hideAndClear}
-            />
-          </div>
-        )}
+            {this.state.selectedTemplate === 3 && (
+              <div>
+                <p>Enter the money need to spend on an order.</p>
+                <TextField
+                  variant="outlined"
+                  type="number"
+                  inputProps={{ min: 1, max: 300 }}
+                  onChange={this.handleXChange}
+                />
+                <SubmitButton onClick={() => this.onPresetComplete('dollars')} />
+              </div>
+            )}
+          </DialogContent>
+        </Dialog>
       </div>
     );
   }
 }
 
 export default PresetTemplates;
+
+function SubmitButton({ onClick }) {
+  return (
+    <div>
+      <Button variant="contained" color="primary" style={{ marginTop: 20 }} onClick={onClick}>
+        Finish
+      </Button>
+    </div>
+  );
+}
