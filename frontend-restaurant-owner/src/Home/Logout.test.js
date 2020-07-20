1bf379f4c08b310f419d00c91fdb4d007e2f7dcf
Min Qi Zhang <52721450+Min-Qi-Zhang@users.noreply.github.com>
2020-07-20 02:25:47 -0400

    Add documentation to Logout test suite (#80)
    * Add documentation for Logout test suite
    
    * Change Logout to Logout button in documentation
    
    * Put high-level description at beginning of file (before imports)

--- frontend-restaurant-owner/src/Home/Logout.test.js
+++ frontend-restaurant-owner/src/Home/Logout.test.js
@@ -1,3 +1,11 @@
+/**
+ * This test suite tests Logout component.
+ * The following aspects are tested:
+ * 1. Logout button is able to render without crashing
+ * 2. Shows a logout button
+ * 3. Calls localStorage clear and location assign when Logout is clicked
+ */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { render, fireEvent } from '@testing-library/react';
@@ -17,17 +25,26 @@ beforeEach(() => {
   });
 });
 
+/**
+ * Renders logout button without crashing
+ */
 it('renders logout button without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Logout />, div);
 });
 
+/**
+ * Shows a logout button
+ */
 it('shows a logout button', () => {
   const { getByTestId } = render(<Logout />);
   expect(getByTestId('logout')).toBeInTheDocument();
 });
 
-it('calls localStorage clear on click', () => {
+/**
+ * Calls localStorage clear and location assign when Logout is clicked
+ */
+it('calls localStorage clear and location assign on click', () => {
   const { getByTestId } = render(<Logout />);
   fireEvent.click(getByTestId('logout'));
   expect(window.localStorage.clear).toBeCalled();
