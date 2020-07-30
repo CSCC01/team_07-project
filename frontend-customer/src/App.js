83cc4091b39292399dd2a0e8ece052a08015d312
1231joe1231 <zhuangruixin1231@gmail.com>
2020-07-30 11:51:55 -0400

    Customer registration (#86)
    * Imeplement basic registration without setting customer role
    
    * Add global axios URL
    
    * Complete role update request
    
    * Fix authorization header issue
    
    * Remove duplicate declaration
    
    * Fix small styling issue
    
    * Fix test case
    
    Configured testing library version and testing command. Passed all tests.

--- frontend-customer/src/App.js
+++ frontend-customer/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Login from './Login';
+import Register from './Register';
 import { Route, Switch } from 'react-router-dom';
 import Home from './Home';
 import PromotionDetails from './PromotionDetails'
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Switch>
         <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
         <Route path="/promotions/:id" component={PromotionDetails} />
         <Route exact path="/" component={Home} />
         <Route component={NoMatch} />
