4bf82dcbadfdf022e6ccc228449c38450cbbc324
Min Qi Zhang <52721450+Min-Qi-Zhang@users.noreply.github.com>
2020-08-03 19:44:46 -0400

    Update App.js

--- frontend-customer/src/App.js
+++ frontend-customer/src/App.js
@@ -12,13 +12,13 @@ function App() {
   return (
     <div className='App'>
       <Switch>
-        <Route path='/login' component={Login} />
-        <Route path='/register' component={Register} />
-        <Route path='/promotions/:id' component={PromotionDetails} />
-        <Route exact path='/' component={Home} />
-        <Route exact path='/coupon-validation' component={CouponValidation} />
-        <Route exact path='/subtask-validation' component={SubtaskValidation} />
-        <Route exact path='/coupon-list' component={CouponList} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route path="/promotions/:id" component={PromotionDetails} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/coupon-validation" component={CouponValidation} />
+        <Route exact path="/subtask-validation" component={SubtaskValidation} />
+        <Route exact path="/coupon-list" component={CouponList} />
         <Route component={NoMatch} />
       </Switch>
     </div>
