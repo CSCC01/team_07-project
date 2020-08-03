c5ebad09cca906d7e53f192fbd6e854b0ff4f4f3
Min Qi Zhang <52721450+Min-Qi-Zhang@users.noreply.github.com>
2020-08-03 19:43:45 -0400

    Change " to ' in App.js

--- frontend-customer/src/App.js
+++ frontend-customer/src/App.js
@@ -1,24 +1,24 @@
-import React from "react";
-import Login from "./Login";
-import Register from "./Register";
-import { Route, Switch } from "react-router-dom";
-import Home from "./Home";
-import PromotionDetails from "./PromotionDetails";
-import CouponValidation from "./CouponValidation";
-import SubtaskValidation from "./SubtaskValidation";
-import CouponList from "./CouponList";
+import React from 'react';
+import Login from './Login';
+import Register from './Register';
+import { Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import PromotionDetails from './PromotionDetails';
+import CouponValidation from './CouponValidation';
+import SubtaskValidation from './SubtaskValidation';
+import CouponList from './CouponList';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Switch>
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-        <Route path="/promotions/:id" component={PromotionDetails} />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/coupon-validation" component={CouponValidation} />
-        <Route exact path="/subtask-validation" component={SubtaskValidation} />
-        <Route exact path="/coupon-list" component={CouponList} />
+        <Route path='/login' component={Login} />
+        <Route path='/register' component={Register} />
+        <Route path='/promotions/:id' component={PromotionDetails} />
+        <Route exact path='/' component={Home} />
+        <Route exact path='/coupon-validation' component={CouponValidation} />
+        <Route exact path='/subtask-validation' component={SubtaskValidation} />
+        <Route exact path='/coupon-list' component={CouponList} />
         <Route component={NoMatch} />
       </Switch>
     </div>
@@ -26,7 +26,7 @@ function App() {
 }
 
 function NoMatch() {
-  return <div>You have come to a place of nowhere! :(</div>;
+  return <div>You have come to a place of nowhere! :(</div>
 }
 
 export default App;
