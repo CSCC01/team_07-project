df0558610f38f3c44a0ad44ba14f83e016292d49
Yifei Yin <yifeiyin@foxmail.com>
2020-08-07 19:35:08 -0400

    Add customized endpoint logic

--- backend/api/promotion/controllers/promotion.js
+++ backend/api/promotion/controllers/promotion.js
@@ -5,4 +5,33 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async findProgress(ctx) {
+    console.log(ctx.state.user);
+    ctx.response.send();
+  },
+  async participate(ctx) {
+    const { id } = ctx.params;
+    const user = ctx.state.user;
+
+    if (user.role.name !== 'Customer') {
+      ctx.response.status = 403;
+      ctx.response.body = { error: 'Promotion participation is only for customers.' };
+      return;
+    }
+
+    const promotion = await strapi.query('promotion').findOne({ id });
+    if (promotion === null) {
+      ctx.response.status = 404;
+      ctx.response.body = {
+        error: 'The specified promotion was not found.',
+      };
+      return;
+    }
+    console.log(promotion.progresses);
+
+    ctx.response.body = {
+      message: 'Successfully created.',
+    };
+  },
+};
