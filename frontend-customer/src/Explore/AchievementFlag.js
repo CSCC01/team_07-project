b1152c6317bd24d447ccb5e694c6f4c669e37862
1231joe1231 <zhuangruixin1231@gmail.com>
2020-08-08 06:54:59 -0400

    Modify flags
    Add pop up to flags and move detail into the popup.
    Add customized color for flags according to their complete number.

--- frontend-customer/src/Explore/AchievementFlag.js
+++ frontend-customer/src/Explore/AchievementFlag.js
@@ -1,16 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
 import FlagIcon from '@material-ui/icons/Flag';
+import Box from '@material-ui/core/Box';
+import Popover from '@material-ui/core/Popover';
+import PopupState, { bindTrigger, bindPopover } from 'material-ui-popup-state';
 
 import './AchievementFlag.css'
 
-export default function AchievementFlag(props){
-    return(
-        <div className='flag'>
-            <FlagIcon fontSize="large"/>
-            <div className='flag-texts'>
-                <p className='flag-text'>{props.restaurantName}</p>
-                <p className='flag-text'>Completed {props.number} promotions</p>
-            </div>
+class AchievementFlag extends Component {
+  state = {
+    color: '',
+  };
+
+  componentDidMount() {
+    if(this.props.number <= 1) {
+      this.setState({
+        color: '#FFDE03'
+      })
+    } else if(2 <= this.props.number && this.props.number <= 4) {
+      this.setState({
+        color: '#0336FF'
+      })
+    } else if(5 <= this.props.number) {
+      this.setState({
+        color: '#FF0266'
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="title-popup-wrapper">
+          <PopupState variant="popover" popupId="demo-popup-popover">
+            {(popupState) => (
+              <div className="title-popup-button">
+                <FlagIcon 
+                  fontSize="large" 
+                  {...bindTrigger(popupState)}
+                  style={{ color: this.state.color }}
+                />
+                <Popover
+                  {...bindPopover(popupState)}
+                  anchorOrigin={{
+                    vertical: 'center',
+                    horizontal: 'right',
+                  }}
+                  transformOrigin={{
+                    vertical: 'center',
+                    horizontal: 'left',
+                  }}
+                >
+                  <Box p={2}>
+                    <div className='flag-texts'>
+                        <p className='flag-text'>{this.props.restaurantName}</p>
+                        <p className='flag-text'>Completed {this.props.number} promotions</p>
+                    </div>
+                  </Box>
+                </Popover>
+              </div>
+            )}
+          </PopupState>
         </div>
-    )
+      </div>
+    );
+  }
 }
+  
+export default AchievementFlag;
