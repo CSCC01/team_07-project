efa416904123715ad619a90d34cc693c40e18911
Min-Qi-Zhang <minqi0303@gmail.com>
2020-08-08 10:41:18 -0400

    Add participate button

--- frontend-customer/src/PromotionDetails/Progress/index.js
+++ frontend-customer/src/PromotionDetails/Progress/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React from "react";
+// import axios from "axios";
+import Button from "@material-ui/core/Button";
 
-export default function Progress(props){
-    let subtasks;
-    if (props.content) subtasks = props.content.map((subtask)=><div>{subtask}</div>);
-    return (
-        <div>
-            <div>Current Progress:</div>
-            {subtasks}
-        </div>
-    );
+export default function Progress(props) {
+  let subtasks;
+  if (props.content)
+    subtasks = props.content.map((subtask) => <div>{subtask}</div>);
+
+  return (
+    <div>
+      <div>Current Progress:</div>
+      {subtasks}
+      <br></br>
+      <div style={{ textAlign: "center" }}>
+        <Button
+          variant="outlined"
+          color="primary"
+          // onClick={postProgress(props.id)}
+        >
+          Participate
+        </Button>
+      </div>
+    </div>
+  );
 }
+
+// const postProgress = async (id) => {
+//   await axios.post("/promotions/" + id + "/participate");
+// };
--- frontend-customer/src/PromotionDetails/index.js
+++ frontend-customer/src/PromotionDetails/index.js
@@ -1,53 +1,55 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
-import { Typography, Paper } from '@material-ui/core';
-import Progress from './Progress';
+import { Carousel } from "react-responsive-carousel";
+import { Typography, Paper } from "@material-ui/core";
+import Progress from "./Progress";
 
-import './index.css'
+import "./index.css";
 
 export default function PromotionDetails(props) {
-    const [data, setData] = useState({});
-    const [images, setImages] = useState([]); // images as a Carousel element
+  const [data, setData] = useState({});
+  const [images, setImages] = useState([]); // images as a Carousel element
 
-    useEffect(() => {
-      const { params: { id } } = props.match;
-      getPromotionDetails(id).then((data) => {
-        setData(data);
-        const carousel = 
-          <Carousel renderThumbs={()=>{}}>
-            {data.image.map(url => 
-            (<div className="imageCutter">
+  useEffect(() => {
+    const {
+      params: { id },
+    } = props.match;
+    getPromotionDetails(id).then((data) => {
+      setData(data);
+      const carousel = (
+        <Carousel renderThumbs={() => {}}>
+          {data.image.map((url) => (
+            <div className="imageCutter">
               <img src={axios.defaults.baseURL + url} alt="promotion img" />
-            </div>))}
-          </Carousel>;
-        setImages(carousel);
-      });
-    }, [props.match])
+            </div>
+          ))}
+        </Carousel>
+      );
+      setImages(carousel);
+    });
+  }, [props.match]);
 
-    return (
-      <>
-        {images}
-        <div className="promotionDetails">
-          <div>
-            <Typography variant="h4" component="h1" className="title">
-              {data.title}
-            </Typography>
-            <Typography>
-              {"Closing Time: " + new Date(data.expired_date).toLocaleString()}
-            </Typography>
-          </div>
-          <Paper className="description">
-            {data.description}
-          </Paper>
-          <Progress content={data.subtask} />
+  return (
+    <>
+      {images}
+      <div className="promotionDetails">
+        <div>
+          <Typography variant="h4" component="h1" className="title">
+            {data.title}
+          </Typography>
+          <Typography>
+            {"Closing Time: " + new Date(data.expired_date).toLocaleString()}
+          </Typography>
         </div>
-      </>
-    );
-  };
+        <Paper className="description">{data.description}</Paper>
+        <Progress content={data.subtask} id={props.match} />
+      </div>
+    </>
+  );
+}
 
 export const getPromotionDetails = async (id) => {
-  const promotionDetails = await axios.get('/promotions/'+ id)
+  const promotionDetails = await axios.get("/promotions/" + id);
   return promotionDetails.data;
-}
+};
