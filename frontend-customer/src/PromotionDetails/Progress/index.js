ebf1afc27369ab8a9210b1ddea59b25c0ac15a65
Min-Qi-Zhang <minqi0303@gmail.com>
2020-08-10 03:48:58 -0400

    Implement functionality show status, send request
    # Conflicts:
    #	frontend-customer/src/PromotionDetails/Progress/index.js

--- frontend-customer/src/PromotionDetails/Progress/index.js
+++ frontend-customer/src/PromotionDetails/Progress/index.js
@@ -2,38 +2,82 @@ import React from "react";
 // import axios from "axios";
 import Button from "@material-ui/core/Button";
 
-export default function Progress(props) {
-  const [participated, setParticipated] = useState(false);
-  const jwtToken = localStorage.getItem("Authorization-Token");
+export default class Progress extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      participated: false,
+      jwtToken: localStorage.getItem("Authorization-Token"),
+      progress: -1,
+    };
+  }
 
-  /** if found progress in database
-   *  setParticipated(true);
-   */
+  async componentDidMount() {
+    this.setState({
+      progress: await getProgress(this.props.id, this.state.jwtToken),
+    });
+    if (this.state.progress !== -1) {
+      this.setState({ participated: true });
+    }
+  }
 
-  let subtasks;
-  if (props.content && !participated)
-    subtasks = props.content.map((subtask) => <div>{subtask}</div>);
+  render() {
+    let subtasks;
+    if (this.props.content && !this.state.participated)
+      subtasks = this.props.content.map((subtask) => <div>{subtask}</div>);
 
-  return (
-    <div onLoad={getProgress(props.id, jwtToken)}>
-      <div>Current Progress:</div>
-      {subtasks}
-      <br></br>
-      <div style={{ textAlign: "center" }}>
-        <Button
-          variant="outlined"
-          color="primary"
-          disabled={participated}
-          onClick={() => {
-            postProgress(props.id, jwtToken);
-            setParticipated(true);
-          }}
-        >
-          Participate
-        </Button>
+    let subtaskValidate;
+    if (this.state.participated) {
+      subtaskValidate = this.state.progress.subtasks.map((subtask) => (
+        <div>
+          {subtask.description}
+          {subtask.status === "ongoing" && (
+            <Button
+              color="primary"
+              size="small"
+              onClick={async () =>
+                await postTaskRequest(
+                  this.props.id,
+                  subtask.index,
+                  this.state.jwtToken
+                )
+              }
+            >
+              validate
+            </Button>
+          )}
+          {subtask.status === "ongoing" && (
+            <div style={{ color: "red" }}>ONGOING</div>
+          )}
+          {subtask.status === "completed" && (
+            <div style={{ color: "green" }}>COMPLETED</div>
+          )}
+        </div>
+      ));
+    }
+
+    return (
+      <div onLoad={getProgress(this.props.id, this.state.jwtToken)}>
+        <div>Current Progress:</div>
+        {subtasks}
+        {subtaskValidate}
+        <br></br>
+        <div style={{ textAlign: "center" }}>
+          <Button
+            variant="contained"
+            color="primary"
+            disabled={this.state.participated}
+            onClick={() => {
+              postProgress(this.props.id, this.state.jwtToken);
+              this.setState({ participated: true });
+            }}
+          >
+            {this.state.participated ? "Participated" : "Participate"}
+          </Button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 const postProgress = async (id, jwtToken) => {
@@ -47,7 +91,7 @@ const postProgress = async (id, jwtToken) => {
 };
 
 const getProgress = async (id, jwtToken) => {
-  var progress;
+  let progress;
   await axios({
     method: "GET",
     url: "/promotions/" + id + "/progress",
@@ -56,22 +100,25 @@ const getProgress = async (id, jwtToken) => {
     },
   })
     .then((response) => {
-      progress = response.status;
+      progress = response.data;
     })
-    .catch((error) => {
+    .catch(() => {
       progress = -1;
     });
   return progress;
 };
 
-// const verifyTask = async (promotion_id, subtask_index) => {
-//   await axios({
-//     method: "POST",
-//     url: "requests",
-//     data: {
-//       type: "subtask",
-//       promotion_id: promotion_id,
-//       subtask_index: subtask_index,
-//     },
-//   });
-// };
+const postTaskRequest = async (promotion_id, subtask_index, jwtToken) => {
+  await axios({
+    method: "POST",
+    url: "requests",
+    data: {
+      type: "subtask",
+      promotion_id: promotion_id,
+      subtask_index: subtask_index,
+    },
+    headers: {
+      Authorization: "Bearer " + jwtToken,
+    },
+  });
+};
